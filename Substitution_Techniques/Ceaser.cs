5365dbea2799584d346d87486f3de6a812f4c1ae
muhammedmokbel <mohamedmokbel97@gmail.com>
2020-02-23 23:29:45 +0200

    ceaser & playfair algorithm

---
+++ Substitution_Techniques/Ceaser.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+namespace Substitution_Techniques
+{
+    public class Ceaser
+    {
+       
+        Dictionary<char, int> lower_alphabet;
+        Dictionary<char, int> upper_alphabet;
+        List<char> map_lower;
+        List<char> map_upper; 
+        int mykey; 
+       public Ceaser()
+        {
+
+            lower_alphabet = new Dictionary<char, int>();
+            upper_alphabet = new Dictionary<char, int>();
+            map_lower = new List<char>();
+            map_upper = new List<char>();
+            mykey = 8; 
+           int index = 0; 
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                lower_alphabet[i] = index;
+                map_lower.Add(i); 
+                index++; 
+            }
+            index = 0; 
+           for (char i = 'A'; i <= 'Z'; i++)
+            {
+                upper_alphabet[i] = index;
+                map_upper.Add(i); 
+                index++;
+            }
+
+               
+        }
+        public string encrypt(string message ,int key)
+        {
+            string encrptedmessage = "";
+            mykey = key; 
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message.ElementAt(i) >= 97)
+                {
+                    int temp = lower_alphabet[message.ElementAt(i)];
+                    temp += mykey;
+                    temp %= 26;
+                    encrptedmessage += map_upper.ElementAt(temp);
+                }
+                else
+                {
+                    int temp = upper_alphabet[message.ElementAt(i)];
+                    temp += mykey;
+                    temp %= 26;
+                    encrptedmessage += map_upper.ElementAt(temp);
+                }
+            }
+            return encrptedmessage; 
+        }
+
+        public string decrypt(string encrptedmassage, int key)
+        {
+            string message = "";
+
+
+            for (int i = 0; i < encrptedmassage.Length; i++)
+            {
+                int temp = upper_alphabet[encrptedmassage.ElementAt(i)];
+                for (int j = 0; j < 26; j++)
+                {
+                    if ((j + key) % 26 == temp)
+                    {
+                        message += map_lower.ElementAt(j);
+                        break; 
+                    }
+                }
+            }
+          
+
+            return message; 
+        }
+
+        public int Analyse(string message, string encrptedmassage)
+        {
+           
+
+            for (int i = 0; i < 26; i++)
+            {
+
+                if (message[0] >= 97)
+                {
+                    int temp = lower_alphabet[message[0]];
+                    if ((temp + i) % 26 == upper_alphabet[encrptedmassage[0]])
+                    {
+                        mykey = i;
+                        break;
+                    }
+                }
+                else
+                {
+
+                    int temp = upper_alphabet[message[0]];
+                    if ((temp + i) % 26 == upper_alphabet[encrptedmassage[0]])
+                    {
+                        mykey = i;
+                        break;
+                    }
+                }
+            }
+
+            return mykey; 
+            
+                
+        }
+
+
+    }
+    
+}
---
+++ Substitution_Techniques/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Substitution_Techniques
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ceaser firstalgo = new Ceaser();
+            string temp = firstalgo.encrypt(textBox1.Text, 29);
+
+            //MessageBox.Show(temp);
+            string temp2 = firstalgo.decrypt(temp, 29);
+            //MessageBox.Show(temp2);
+            int key = firstalgo.Analyse(temp2, temp);
+            //MessageBox.Show(key.ToString());
+
+            Playfair secondalgo = new Playfair();
+          string dec =   secondalgo.encrypt(textBox1.Text, "playfairexample");
+          
+          secondalgo.decrypt(dec, "playfairexample"); 
+
+        }
+    }
+}
